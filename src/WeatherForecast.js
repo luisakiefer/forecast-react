e6f78a2250ea36fbba6fee0f27c9c8ded7fddce4
Luísa Kiefer <91638544+luisakiefer@users.noreply.github.com>
2022-06-24 08:44:11 -0300

    fixed return

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,32 +18,31 @@ export default function WeatherForecast(props) {
   }
 
   if (loaded) {
-
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function(dailyForecast, index) {
+          {forecast.map(function (dailyForecast, index) {
             if (index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
-          
         </div>
       </div>
     );
+  } else {
+    let apiKey = "dfb7d1f03e3eb9184605492c12826c7d";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-  } else { 
-  let apiKey = "dfb7d1f03e3eb9184605492c12826c7d";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
 
-  axios.get(apiUrl).then(handleResponse);
-
-  return null;
+    return null;
   }
 }
